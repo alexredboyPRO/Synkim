0c0743bf5f07ef3b586f8b20ddaaa2b26ce66f67
AlexRedBoy <51050364+alexredboyPRO@users.noreply.github.com>
2025-10-23 02:23:03 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,32 +1,37 @@
-// server/index.js
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
-const server = http.createServer(app);
+app.use(cors());
 
+const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "*" }
+  cors: {
+    origin: "*",
+  },
 });
 
-io.on("connection", socket => {
-  console.log("sock connected:", socket.id);
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
 
-  socket.on("join_room", room => {
-    socket.join(room);
-    console.log(`${socket.id} joined ${room}`);
+  socket.on("play", (time) => {
+    socket.broadcast.emit("play", time);
   });
 
-  socket.on("sync_action", data => {
-    console.log("sync_action from", socket.id, data);
-    io.to(data.room).emit("sync_action", { action: data.action, time: data.time });
+  socket.on("pause", (time) => {
+    socket.broadcast.emit("pause", time);
+  });
+
+  socket.on("sync", (time) => {
+    socket.broadcast.emit("sync", time);
   });
 
   socket.on("disconnect", () => {
-    console.log("disconnect:", socket.id);
+    console.log("User disconnected:", socket.id);
   });
 });
 
-const PORT = 3001;
-server.listen(PORT, () => console.log("Server listening on", PORT));
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
