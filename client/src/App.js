f7d24e931a9492ebe3f8b9ba53c4fe05f0348bb6
AlexRedBoy <51050364+alexredboyPRO@users.noreply.github.com>
2025-10-23 01:39:56 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 
 const SOCKET_URL = "https://synkim.onrender.com/"; // use your tunnel link
 const ROOM = "room1";
-const SYNC_THRESHOLD = 2;
+const SYNC_THRESHOLD = 20;
 
 export default function App() {
   const socketRef = useRef(null);
